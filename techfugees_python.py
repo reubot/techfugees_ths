4b8ef9b4ef46d0ede2d2aef7782c925a02c453a8
reuben <reuben_p@yahoo.com>
2017-03-19 12:22:08 +1000

    simple levenshtein matching

--- techfugees_python.py
+++ techfugees_python.py
@@ -5,11 +5,22 @@ Documentation, License etc.
 '''
 import json
 import pprint
+import Levenshtein
+
 mentor_json=open("mentor.json").read()
 mentee_json=open("mentee.json").read()
 
 mentor_data = json.loads(mentor_json)
 mentee_data = json.loads(mentee_json)
 
-pprint.pprint(mentor_data)
+pprint.pprint(mentor_data[0])
 pprint.pprint(mentee_data)
+
+for mentee in mentee_data:
+    print(mentee)
+    print(mentee["Goal"])
+    for mentor in mentor_data:
+        print(mentor)
+        print(Levenshtein.distance(mentee["Goal"],mentor["Goal"]))
+        print(Levenshtein.distance(mentee["Language"],mentor["Language"]))
+        
