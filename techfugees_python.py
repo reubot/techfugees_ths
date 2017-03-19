85de1f8bc9b84ce733004371b4c15682cac9aff1
reuben <reuben_p@yahoo.com>
2017-03-19 14:37:42 +1000

    only print out best match

--- techfugees_python.py
+++ techfugees_python.py
@@ -19,11 +19,20 @@ mentee_data = json.loads(mentee_json)
 for mentee in mentee_data:
     print(mentee)
 #    print(mentee["Goal"])
+    best_mentor = dict()
+    best_mentor_score = 999
     for mentor in mentor_data:
-        print(mentor)
- #       print("Goal Match: ",100-Levenshtein.distance(mentee["Goal"],mentor["Goal"]))
-        print("Help Match:\t",100-Levenshtein.distance(mentee["Help Need"],mentor["Help Offered"]))
-        print("Language Match:\t",100-Levenshtein.distance(mentee["Language"],mentor["Language"]),)
+#        print(mentor)
+        help_score = Levenshtein.distance(mentee["Help Need"],mentor["Help Offered"]);
+         #       print("Goal Match: ",100-Levenshtein.distance(mentee["Goal"],mentor["Goal"]))
+        #print("Help Match:\t",100-help_score)
+        #print("Language Match:\t",100-Levenshtein.distance(mentee["Language"],mentor["Language"]),)
 #        print("Strengths Match: ",100-Levenshtein.distance(mentee["Strengths"],mentor["Strengths"]),)
 #        print("Interests Match: ",100-Levenshtein.distance(mentee["Interests"],mentor["Interests"]),)
+        if help_score < best_mentor_score:
+            best_mentor = mentor
+            best_mentor_score = help_score
+    print (best_mentor)
+    print("Help Match:\t",100-best_mentor_score)
+
         
